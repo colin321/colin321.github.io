33b7ccbf7ae443d806f412d1c694657a84a143bc
Colin Ciesla <cjcxtd@mail.missouri.edu>
2018-06-18 19:47:32 -0500

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).on('click',function(){
+  $('.collapse').collapse('hide');
+})
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
