ebae20769145ac1a96dd93c5100628d26535eb60
Colin Ciesla <colinciesla@gmail.com>
2018-12-18 21:26:59 -0600

    Delete index.js

--- js/index.js
+++
@@ -1,8 +0,0 @@
-$(document).on('click',function(){
-  $('.collapse').collapse('hide');
-})
-
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
